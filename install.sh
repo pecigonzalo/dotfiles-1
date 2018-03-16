b843c0be22c76def2be4f815c0d33a71db92c33f
Simon Smith <simonsmith@users.noreply.github.com>
2018-03-16 17:24:50 +0000

    wip

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 source ./scripts/homebrew.sh
-source ./scripts/cask.sh
 source ./scripts/zsh.sh
 source ./scripts/symlinks.sh
-source ./scripts/vim.sh
 source ./scripts/node.sh
-source ./scripts/ruby.sh
 source ./scripts/misc.sh
 
 echo "Sourcing .zshrc ..."
--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -14,17 +14,6 @@ brew update
 brew install coreutils
 ln -sv /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
-# Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
-
-# Replace system curl
-brew install curl --with-openssl
-brew link curl --force
-
 # Install zsh
 brew install zsh
 brew install zsh-syntax-highlighting
@@ -34,58 +23,5 @@ brew install zplug
 # less highlighting
 brew install source-highlight
 
-# Install `wget` with IRI support.
-brew install wget --with-iri
-
-# neovim
-brew install python3
-brew install neovim/neovim/neovim
-pip3 install neovim
-
-# fzf
-brew install fzf
-yes | /usr/local/opt/fzf/install
-
-# Ag - fzf is configured to use this
-brew install the_silver_searcher
-
-# tmux
-brew install reattach-to-user-namespace
-brew install tmux
-brew install urlview
-
-# git
-brew install cdiff
-brew install git
-brew install scmpuff
-brew install diff-so-fancy
-
-# Install more recent versions of some OS X tools.
-brew tap homebrew/dupes
-brew install grep --with-default-names
-
-# Ruby
-brew install rbenv
-brew install ruby-build
-
-# Window manager
-brew install koekeishiya/formulae/khd
-brew tap crisidev/homebrew-chunkwm
-brew install chunkwm
-
-# Install other useful binaries.
-brew install ack
-brew install ctags
-brew install direnv
-brew install go
-brew install exa
-brew install fpp
-brew install htop
-brew install httpie
-brew install hugo
-brew install p7zip
-brew install python
-brew install redis
-brew install youtube-dl
 
 brew cleanup
